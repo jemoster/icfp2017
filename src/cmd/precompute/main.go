625c2b082280761d49916f39b95bf6520ef2cf3f
Michael Pratt <michael@pratt.im>
2017-08-04 08:21:29 -0700

    Build graph

--- src/cmd/precompute/main.go
+++ src/cmd/precompute/main.go
@@ -13,15 +13,64 @@ import (
 func readMap() (*protocol.Map, error) {
 	b, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to read map: %v", err)
+		return nil, fmt.Errorf("failed to read map: %v", err)
 	}
 
 	m := &protocol.Map{}
 	if err := json.Unmarshal(b, m); err != nil {
-		return nil, fmt.Errorf("Failed to unmarshal map: %v", err)
+		return nil, fmt.Errorf("failed to unmarshal map: %v", err)
 	}
 
 	return m, nil
+}
+
+type site struct {
+	id protocol.SiteID
+
+	visited  bool
+	distance uint64
+
+	neighbors map[protocol.SiteID]*site
+}
+
+type graph map[protocol.SiteID]*site
+
+func buildGraph(m *protocol.Map) (graph, error) {
+	g := make(graph)
+
+	for _, si := range m.Sites {
+		if s, ok := g[si.ID]; ok {
+			return nil, fmt.Errorf("found duplicate site %+v for %+v", s, si)
+		}
+
+		g[si.ID] = &site{
+			id:        si.ID,
+			neighbors: make(map[protocol.SiteID]*site),
+		}
+	}
+
+	for _, r := range m.Rivers {
+		source, ok := g[r.Source]
+		if !ok {
+			return nil, fmt.Errorf("source missing for river %+v", r)
+		}
+
+		target, ok := g[r.Target]
+		if !ok {
+			return nil, fmt.Errorf("target missing for river %+v", r)
+		}
+
+		source.neighbors[target.id] = target
+		target.neighbors[source.id] = source
+	}
+
+	for _, mine := range m.Mines {
+		if _, ok := g[mine]; !ok {
+			return nil, fmt.Errorf("mine %+v missing in graph %+v", mine, g)
+		}
+	}
+
+	return g, nil
 }
 
 func main() {
@@ -31,5 +80,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Map: %+v\n", m)
+	fmt.Printf("Parsed map: %+v\n", m)
+
+	g, err := buildGraph(m)
+	if err != nil {
+		log.Printf("Failed to build graph: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Built graph: %+v\n", g)
 }
