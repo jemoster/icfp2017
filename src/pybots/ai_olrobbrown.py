b49050351c9656d568e78961171c40def9ce07d1
Johnathan Free Wortley <jwortley@uber.com>
2017-08-05 16:50:14 -0700

    Begin adding Djikstras to Ol Doc Brown

--- src/pybots/ai_olrobbrown.py
+++ src/pybots/ai_olrobbrown.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3 -u
 import random
+from base_bot import PyBot
+from astar import graph_neighbors
 from base_bot import PyBot, log
 
 
@@ -8,6 +10,8 @@ class OlRobBrownBot(PyBot):
         p = setup['punter']
 
         possible_claims = []
+        rivers = setup['map']['rivers']
+        log(setup['map']['rivers'])
         for river in setup['map']['rivers']:
             possible_claims.append((river['source'], river['target']))
 
@@ -16,6 +20,13 @@ class OlRobBrownBot(PyBot):
         # Start with first mine
         available_mines = setup['map']['mines']
         start_mine = random.choice(available_mines)
+        target_mine = None
+
+        if len(available_mines) > 1:
+            target_mine = random.choice(available_mines)
+            while target_mine == start_mine:
+                target_mine = random.choice(available_mines)
+
         available_mines.remove(start_mine)
         prev_sites = [start_mine]
         log("starting w/ {}".format(prev_sites))
@@ -26,12 +37,41 @@ class OlRobBrownBot(PyBot):
                 'possible_claims': possible_claims,
                 'punter_id': punter_id,
                 'available_mines': available_mines,
-                'prev_sites': prev_sites,
+                'target_mine': target_mine,
+                'rivers': rivers,
+                'prev_sites':prev_sites
             }
         }
 
-    def choose_first_connected(self, claims, prev_sites, last_end):
-        for claim in claims:
+    def get_neighbors(self, claims, prev_sites):
+
+        def func(currentNode):
+            def get_possible_neighbors(n):
+
+                for claim in claims:
+                    if claim[0] == n:
+                        if not claim[1] in prev_sites:
+                            return claim[1], claim
+                    if claim[1] == currentNode:
+                        if not claim[0] in prev_sites:
+                            return claim[0], claim
+                return None, None
+
+            possible_paths = list(filter(get_possible_neighbors, claims))
+            return possible_paths
+        return func
+
+    def determine_best_move(self, state, last_end):
+        target_mine = state['target_mine']
+        possible_claims = state['possible_claims']
+        prev_sites = state['prev_sites']
+
+        if target_mine is not None:
+            pass
+
+        choices = graph_neighbors(target_mine, possible_claims, last_end)
+
+        for claim in possible_claims:
             if claim[0] == last_end:
                 if not claim[1] in prev_sites:
                     return claim[1], claim
@@ -51,6 +91,8 @@ class OlRobBrownBot(PyBot):
         punter_id = msg['state']['punter_id']
         available_mines = msg['state']['available_mines']
         prev_sites = msg['state']['prev_sites']
+        target_mine = msg['state']['target_mine']
+        state = msg['state']
 
         for move in msg['move']['moves']:
             if 'pass' in move:
@@ -59,18 +101,18 @@ class OlRobBrownBot(PyBot):
             possible_claims.remove([move['source'], move['target']])
 
         last_idx = -1
-        next_site, claim = self.choose_first_connected(possible_claims, prev_sites, prev_sites[last_idx])
+        next_site, claim = self.determine_best_move(state, prev_sites[last_idx])
         while claim is None:
             log("no options moving from {} in moves {}".format(prev_sites[last_idx], possible_claims))
             last_idx -= 1
             if -last_idx > len(prev_sites):
                 break
-            next_site, claim = self.choose_first_connected(possible_claims, prev_sites, prev_sites[last_idx])
+            next_site, claim = self.determine_best_move(state, prev_sites[last_idx])
 
         if claim is None and available_mines:
             next_mine = random.choice(available_mines)
             available_mines.remove(next_mine)
-            next_site, claim = self.choose_first_connected(possible_claims, prev_sites, next_mine)
+            next_site, claim = self.determine_best_move(state, next_mine)
 
         if claim is None:
             log("outta options, moves left: {}".format(possible_claims))
@@ -91,7 +133,8 @@ class OlRobBrownBot(PyBot):
                 'possible_claims': possible_claims,
                 'punter_id': punter_id,
                 'available_mines': available_mines,
-                'prev_sites': prev_sites
+                'prev_sites': prev_sites,
+                'target_mine': target_mine
             }
         }
 
