b645bbcc04f4fc224785e3ff06d0cb772633e37f
Alex Kesling <alex.kesling@gmail.com>
2017-08-05 18:22:56 -0400

    Brownian bot now claims available mine-rivers

--- src/bots/akesling/brownian/main.go
+++ src/bots/akesling/brownian/main.go
@@ -17,6 +17,7 @@ type state struct {
 	Punters uint64
 	Map     protocol.Map
 	OwnedPaths [][]protocol.Site
+	AvailableMineRivers []protocol.River
 
 	Turn uint64
 }
@@ -45,7 +46,7 @@ func UpdateGraph(g *simple.UndirectedGraph, m []protocol.Move) {
 		if move.Claim != nil {
 			claimedEdge := g.EdgeBetween(
 				g.Node(int64(move.Claim.Source)),
-				g.Node(int64(move.Claim.Target))).(graph.MetadataEdge)
+				g.Node(int64(move.Claim.Target))).(*graph.MetadataEdge)
 			claimedEdge.IsOwned = true
 			claimedEdge.Punter = move.Claim.Punter
 		}
@@ -67,6 +68,22 @@ func (Brownian) Setup(setup *protocol.Setup) (*protocol.Ready, error) {
 	glog.Infof("Setup")
 
 	s := InitializeState(setup)
+	g := graph.Build(&s.Map)
+
+	// Add all mine-neighboring rivers to AvailableMineRivers
+	for i := range s.Map.Mines {
+		mine := s.Map.Mines[i]
+		neighbors := g.From(g.Node(int64(s.Map.Mines[i])))
+		for j := range neighbors {
+			n := neighbors[j]
+			s.AvailableMineRivers = append(
+				s.AvailableMineRivers,
+				protocol.River{
+					Source: mine,
+					Target: protocol.SiteID(n.ID()),
+				})
+		}
+	}
 
 	return &protocol.Ready{
 		Ready: s.Punter,
@@ -85,9 +102,43 @@ func (Brownian) Play(m []protocol.Move, jsonState json.RawMessage) (*protocol.Ga
 	g := graph.Build(&s.Map)
 	UpdateGraph(g, m)
 	s.Update(g, m)
-
 	glog.Infof("Turn: %d", s.Turn)
 
+	// OwnedPaths [][]protocol.Site
+	// AvailableMineRivers []protocol.River
+	if len(s.AvailableMineRivers) != 0 {
+		// Grab all rivers around mines.
+		i := 0
+		for i < len(s.AvailableMineRivers) {
+			candidate := s.AvailableMineRivers[i]
+			edge := g.EdgeBetween(g.Node(int64(candidate.Source)), g.Node(int64(candidate.Target))).(*graph.MetadataEdge)
+			i++
+
+			if !edge.IsOwned {
+				s.AvailableMineRivers = s.AvailableMineRivers[i:]
+				s.OwnedPaths = append(s.OwnedPaths, []protocol.Site{protocol.Site{ID: candidate.Source}, protocol.Site{ID: candidate.Source}})
+
+				return &protocol.GameplayOutput{
+					Move: protocol.Move{
+						Claim: &protocol.Claim{
+							Punter: s.Punter,
+							Source: candidate.Source,
+							Target: candidate.Target,
+						},
+					},
+					State: s,
+				}, nil
+			}
+		}
+
+		s.AvailableMineRivers = make([]protocol.River, 0)
+	}
+
+	if len(s.AvailableMineRivers) == 0 {
+		// Randomly extend our rivers.
+	}
+
+	// No other moves were made, pass.
 	return &protocol.GameplayOutput{
 		Move: protocol.Move{
 			Pass: &protocol.Pass{
