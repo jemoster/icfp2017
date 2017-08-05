303aedccfab4812832283378198b877851723627
Joseph Moster <jemoster@gmail.com>
2017-08-05 15:26:11 -0400

    improved make player options

--- tools/bot_runner/make_player_data.py
+++ tools/bot_runner/make_player_data.py
@@ -8,11 +8,11 @@ from time import time
 
 server = "punter.inf.ed.ac.uk"
 
-bot_list = [
-    "python -u src/pybots/ai_olrobbrown.py",
-    "python -u src/pybots/ai_random.py",
+known_bots = {
+    'brown': "python -u src/pybots/ai_olrobbrown.py",
+    'random': "python -u src/pybots/ai_random.py",
     # "go run src/cmd/play/main.go",
-]
+}
 
 def main():
     parser = argparse.ArgumentParser(description='ICFP 2017 Online Adapter')
@@ -20,15 +20,28 @@ def main():
     parser.add_argument('bot_count', action="store", type=int, help='how many bots total')
     parser.add_argument('port', action="store", type=int, help='Port of the competition server '
                         'see http://punter.inf.ed.ac.uk/status.html for details')
+    parser.add_argument('--bot', action='append', default=[], help='Add bots that you want tested')
+    parser.add_argument('--default', action='store', default=None, help='default bot to use')
     parser.add_argument('--server', action="store", default="punter.inf.ed.ac.uk",
                         help='The game server to connect to defaults to "punter.inf.ed.ac.uk"')
     results = parser.parse_args()
 
+    bot_list = results.bot
+
     procs = []
     run_time = time()
     for num in range(results.bot_count):
-        bot = random.choice(bot_list)
-        basename = '{}_{}_{}_.1.txt'.format(results.port, run_time, num)
+        if bot_list:
+            bot = bot_list.pop()
+        elif results.default:
+            bot = results.default
+        else:
+            bot = random.choice(list(known_bots.keys()))
+
+        if bot in known_bots:
+            bot = known_bots[bot]
+
+        basename = '{}_{}_{}_'.format(results.port, run_time, num)
         filename = os.path.join('data', 'multibot', basename)
         cmd = " ".join(["python", "-u", "tools/bot_runner/online_adapter.py", '"{}"'.format(bot), str(results.port), '--record', filename])
         print('calling: ', cmd)
---
+++ tools/bot_runner/print_metrics.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+from os import listdir
+from os.path import isfile, join
+
+from adapter import get_metrics
+
+
+def main():
+    parser = argparse.ArgumentParser(description='ICFP 2017 Metrics Generator')
+
+    parser.add_argument('dir', action="store", type=str, help='directory of playlogs')
+    results = parser.parse_args()
+
+    playlogs = [join(results.dir, f) for f in listdir(results.dir) if isfile(join(results.dir, f))]
+    for log in playlogs:
+        try:
+            stats = get_metrics(log)
+            print('{}/{} \t{} \t{log}'.format(log=log, *stats))
+        except:
+            pass
+            # print("couldn't parse:", log)
+
+
+if __name__ == '__main__':
+    main()
