f65397a2bf640340791e7b6baa77f540e6e7816f
Joseph Moster <jemoster@gmail.com>
2017-08-05 11:32:29 -0400

    Cleaned up subclassing in the pybots

--- src/pybots/base_bot.py
+++ src/pybots/base_bot.py
@@ -1,12 +1,10 @@
+from __future__ import print_function
 import sys
 import json
-import random
 
 
-def print_err(msg):
-    """ Print a message to stderr to keep stdout clear"""
-    sys.stderr.write("{}\n".format(msg))
-    sys.stderr.flush()
+def log(s):
+    print(s, file=sys.stderr)
 
 
 class PyBot:
@@ -37,51 +35,10 @@ class PyBot:
         sys.stdout.flush()
 
     def setup(self, setup):
-        p = setup['punter']
-
-        possible_claims = []
-        for river in setup['map']['rivers']:
-            possible_claims.append((river['source'], river['target']))
-
-        punter_id = setup['punter']
-
-        return {
-            'ready': p,
-            'state': {
-                'possible_claims': possible_claims,
-                'punter_id': punter_id
-            }
-        }
+        raise NotImplementedError
 
     def gameplay(self, msg):
-        if 'stop' in msg:
-            return {}
-
-        if 'timeout' in msg:
-            return {}
-
-        possible_claims = msg['state']['possible_claims']
-        punter_id = msg['state']['punter_id']
-
-        for move in msg['move']['moves']:
-            if 'pass' in move:
-                continue
-            move = move['claim']
-            possible_claims.remove([move['source'], move['target']])
-
-        claim = random.choice(possible_claims)
-
-        return {
-            'claim': {
-                'punter': punter_id,
-                'source': claim[0],
-                'target': claim[1]
-            },
-            'state': {
-                'possible_claims': possible_claims,
-                'punter_id': punter_id
-            }
-        }
+        raise NotImplementedError
 
     def _read_structured(self):
         while ':' not in self.buffer:
