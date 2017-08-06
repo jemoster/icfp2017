22a7f13b109eaabfd627c1beea9c37c001a8255b
Alex Ray <a@machinaut.com>
2017-08-05 22:00:36 -0700

    rolling updates to the idle runner

--- scripts/idle_launcher.sh
+++ scripts/idle_launcher.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for i in $(seq 1 10); do
-    echo 'launching' $i
-    make IDLENAME=idle-$i idle-run
+while true; do
+    for i in $(seq 1 200); do
+        echo 'killing' $i
+        docker kill idle-$i
+        echo 'removing' $i
+        docker rm idle-$i
+        echo 'building' $i
+        make build
+        echo 'launching' $i
+        make IDLENAME=idle-$i idle-run
+    done
 done
