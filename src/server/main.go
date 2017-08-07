6369839f42320d1b37932ff0387303970510ea2d
Unremarkable <andrew.hausch@gmail.com>
2017-08-06 19:57:35 -0700

    Added support for Options and Splurges, enabled by default.

---
+++ src/server/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/jemoster/icfp2017/src/protocol"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func loadMap(path string) (*protocol.Map, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	m := new(protocol.Map)
+
+	err = json.Unmarshal(data, m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func main() {
+	mapPath := flag.String("map", "", "[required] A JSON file")
+	srvPort := flag.Int("port", 9001, "The port to listen on")
+	numPunters := flag.Int("punters", 2, "Number of players")
+
+	splurges := flag.Bool("splurges", true, "to disable splurges use --splurges=false")
+	options := flag.Bool("options", true, "to disable options use --options=false")
+
+	flag.Parse()
+
+	if len(*mapPath) < 1 {
+		log.Fatal("map can not be undefined")
+	}
+
+	mapData, err := loadMap(*mapPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Map Loaded (from %q)\n", *mapPath)
+	fmt.Printf("  Sites:  %d\n", len(mapData.Sites))
+	fmt.Printf("  Rivers: %d\n", len(mapData.Rivers))
+	fmt.Printf("  Mines:  %d\n", len(mapData.Mines))
+
+	serv := Server{
+		Map:        *mapData,
+		Port:       *srvPort,
+		NumPunters: *numPunters,
+		Settings: protocol.Settings{
+			Futures:  false,
+			Splurges: *splurges,
+			Options:  *options,
+		},
+	}
+
+	serv.run()
+}
